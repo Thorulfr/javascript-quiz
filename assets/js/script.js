8ea523272cea3a9cfb5ef4e1614c524d79d9ade0
Ben H <deepfreespace@gmail.com>
2021-10-08 22:32:58 -0600

    Added questions array, got answer buttons to display.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
+var questions = [
+    "<h1>The condition in an if/else statement is enclosed with:</h1>",
+    "<h1>A variable inside of a function can be used by other functions.</h1>",
+    "<h1>How do you add comments to JavaScript?</h1>",
+    "<h1>A 'switch' statement must always include a default option.</h1>",
+    "<h1>How do you increment an integer variable in JavaScript?</h1>",
+    "<h1>'Const' variables' values can be reassigned.</h1>"
+];
+
+var questionNumber = 0;
+
 // When the start button is clicked, start the quiz
 $("#start-button").click(function() {
-    $("#start-button").remove();
-    $("#quiz-header").html("<h1>The condition in an if/else statement is enclosed with:</h1>");
-    $("#quiz-body").html("<button type='button' class='btn btn-primary' id='start-button'>Start Quiz</button>");
+    $("#start-button").addClass("d-none");
+    $("#instructions").addClass("d-none");
+    $("#quiz-header").html(questions[0]);
+    $("#answer-one").removeClass("d-none");
+    $("#answer-one").addClass("d-inline-block");
+    $("#answer-two").removeClass("d-none");
+    $("#answer-two").addClass("d-inline-block");
+    $("#answer-three").removeClass("d-none");
+    $("#answer-three").addClass("d-inline-block");
+    $("#answer-four").removeClass("d-none");
+    $("#answer-four").addClass("d-inline-block");
 })
